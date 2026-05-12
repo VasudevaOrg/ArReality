6c8757e0ed2e3043facb35acde69946e0da2fc34
VasudevaSurf <vasudev@surf.mt>
2026-05-12 22:34:14 +0530

    Update ARScreen.js

--- src/screens/ARScreen.js
+++ src/screens/ARScreen.js
@@ -17,6 +17,8 @@ import {
   ViroDirectionalLight
 } from '@reactvision/react-viro';
 
+ento idhi
+
 // This is the actual AR Scene that runs inside the ViroARSceneNavigator
 const MonumentARScene = (props) => {
   const { monument } = props.sceneNavigator.viroAppProps;
